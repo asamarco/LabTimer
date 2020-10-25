dd0ee986c8265d3cd5f6f6ffef80b71a2028d6b3
Marco <marcoasa90@gmail.com>
2020-10-25 16:29:01 +0100

    Changed minimum SDK version to 25, tested and fixed on Android Nougat 7.1.2

--- src/main/java/com/example/labtimer/receivers/TimerExpiredReceiver.kt
+++ src/main/java/com/example/labtimer/receivers/TimerExpiredReceiver.kt
@@ -3,6 +3,8 @@ package com.example.labtimer.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Vibrator
 import android.util.Log
 import com.example.labtimer.utils.AlarmUtils
 import com.example.labtimer.utils.NotificationUtils
@@ -12,6 +14,10 @@ import com.example.labtimer.utils.TimerUtils
 class TimerExpiredReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            val vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            vibrator.vibrate(1000) // Vibrate method for below API Level 26
+        }
         NotificationUtils.showTimerExpired(context)
         AlarmUtils.setAlarmTime(0, 0, context)
         Log.i("labtimer","Broadcast received")
